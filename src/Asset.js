8fbb6e791d51d1a14a789b8513db8b0040d2ebc4
Kildyt <kildyt@gmail.com>
2019-06-17 14:29:10 +0200

    Removed deasync dependency
    https://github.com/parcel-bundler/parcel/issues/2245

--- src/Asset.js
+++ src/Asset.js
@@ -5,7 +5,6 @@ const fs = require('@parcel/fs');
 const md5 = require('./utils/md5');
 const isURL = require('./utils/is-url');
 const config = require('./utils/config');
-const syncPromise = require('./utils/syncPromise');
 const logger = require('@parcel/logger');
 const Resolver = require('./Resolver');
 const objectHash = require('./utils/objectHash');
@@ -125,13 +124,6 @@ class Asset {
       .generateBundleName();
 
     return URL.format(parsed);
-  }
-
-  get package() {
-    logger.warn(
-      '`asset.package` is deprecated. Please use `await asset.getPackage()` instead.'
-    );
-    return syncPromise(this.getPackage());
   }
 
   async getPackage() {
